ea36f97a0e72e88ba035ce2bd23d047204195786
Artem Schubin <a.schubin13@gmail.com>
2017-05-11 11:57:47 +0200

    [EH] Create TDD Game of Life + Specs.

---
+++ app/src/main/java/ar_ubin/gameoflife/GameOfLife.java
@@ -0,0 +1,75 @@
+package ar_ubin.gameoflife;
+
+
+import android.os.Handler;
+
+class GameOfLife
+{
+    private final Handler myOffMainThreadHandler;
+    private GameWorld mWorld;
+    private boolean mIsRunning = true;
+
+    public GameOfLife( GameWorld world ) {
+        mWorld = world;
+        myOffMainThreadHandler = new Handler();
+    }
+
+    public GameWorld getWorld() {
+        return mWorld;
+    }
+
+    public GameWorld nextGeneration() {
+        GameWorld nextWorld = mWorld.nextIteration();
+        mWorld = nextWorld;
+
+        return mWorld;
+    }
+
+    public void runOnce() {
+        nextGeneration();
+    }
+
+    public void pause() {
+        mIsRunning = false;
+    }
+
+    public void run( final GameView gameView ) {
+        mIsRunning = true;
+
+        Runnable runnableOffMain = new Runnable()
+        {
+            @Override
+            public void run() {  // this thread is not on the main
+
+                while( mIsRunning ) {
+
+                    generationLifeSpan();
+
+                    myOffMainThreadHandler.post( new Runnable()
+                    {  // this is on the main thread
+
+                        public void run() {
+                            mWorld = nextGeneration();
+                            gameView.setNextGeneration( mWorld );
+                        }
+                    } );
+                }
+            }
+        };
+
+        new Thread( runnableOffMain ).start();
+    }
+
+    private void generationLifeSpan() {
+        try {
+            Thread.sleep( 100 );
+        } catch( InterruptedException e ) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isRunning(){
+        return mIsRunning;
+    }
+}
---
+++ app/src/test/java/ar_ubin/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,85 @@
+package ar_ubin.gameoflife;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class GameOfLifeTest
+{
+    @Test
+    public void aSingleCellShouldDieInNextIteration() throws Exception {
+        //given
+        GameWorld world = new GameWorld();
+        world.addCell( new Cell( 0, 0 ) );
+        GameOfLife game = new GameOfLife( world );
+
+        //when
+        GameWorld nextWorld = game.nextGeneration();
+
+        //then
+        assertFalse( nextWorld.isAlive( new Cell( 0, 0 ) ) );
+    }
+
+    @Test
+    public void aCellWithTwoNeighboursShouldLiveInNextIteration() {
+        //given
+        GameWorld world = new GameWorld();
+        world.addCell( new Cell( 0, 1 ) );
+        world.addCell( new Cell( 1, 1 ) );
+        world.addCell( new Cell( 1, 0 ) );
+        GameOfLife game = new GameOfLife( world );
+
+        //when
+        GameWorld nextWorld = game.nextGeneration();
+
+        //then
+        assertTrue( nextWorld.isAlive( new Cell( 1, 1 ) ) );
+    }
+
+    @Test
+    public void aCellWithThreeNeighboursShouldLiveInNextIteration() {
+        //given
+        GameWorld world = new GameWorld();
+        world.addCell( new Cell( 0, 1 ) );
+        world.addCell( new Cell( 1, 1 ) );
+        world.addCell( new Cell( 1, 0 ) );
+        world.addCell( new Cell( 0, 0 ) );
+        GameOfLife game = new GameOfLife( world );
+
+        //when
+        GameWorld nextWorld = game.nextGeneration();
+
+        //then
+        assertTrue( nextWorld.isAlive( new Cell( 1, 1 ) ) );
+    }
+
+    @Test
+    public void aCellWithMoreThanThreeNeighboursShouldDieInNextIteration() {
+        //given
+        GameOfLife game = new GameOfLife( GameWorldTest.createWorld( 3, 3 ) );
+
+        //when
+        GameWorld nextWorld = game.nextGeneration();
+
+        //then
+        assertFalse( nextWorld.isAlive( new Cell( 1, 1 ) ) );
+    }
+
+    @Test
+    public void aDeadCellWithThreeNeighboursShouldBeRebornInNextIteration() {
+        //given
+        GameWorld world = new GameWorld();
+        world.addCell( new Cell( 0, 1 ) );
+        world.addCell( new Cell( 1, 1 ) );
+        world.addCell( new Cell( 1, 0 ) );
+        GameOfLife game = new GameOfLife( world );
+
+        //when
+        GameWorld nextWorld = game.nextGeneration();
+
+        //then
+        assertTrue( nextWorld.isAlive( new Cell( 0, 0 ) ) );
+    }
+}
